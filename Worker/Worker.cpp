51e8b0fb50d518680d08ba61e7aa340f372c96f8
FeverDark <andrej-68689@mail.ru>
2023-04-11 18:16:16 +0300

    Interface realisation

--- Worker/Worker.cpp
+++ Worker/Worker.cpp
@@ -2,13 +2,29 @@
 #include "framework.h"
 #include "Worker.h"
 
+std::vector<Man*> _tempdb_;
+//std::vector<Diagnosis> _tempdiagnosesdb_;
+
 Man::Man() {}
 Man::~Man() {}
 
-Patient::Patient() {}
+Patient::Patient(int id = 0, std::wstring name = L"", std::wstring dob = L"", Diagnosis d = Diagnosis()) {
+    this->id = id;
+    this->name = name;
+    this->dob = dob;
+    if (d.text != L"", d.date != L"",d.workerid != 0) {
+        this->a.push_back(d);
+    }
+}
 Patient::~Patient() {}
 
-Worker::Worker() {}
+Worker::Worker(int id = 0, std::wstring name = L"", std::wstring job = L"", std::string login = "", std::string password = "") {
+    this->id = id;
+    this->name = name;
+    this->job = job;
+    this->login = login;
+    this->password = password;
+}
 Worker::~Worker() {}
 
 const char* creatingTables = "CREATE TABLE IF NOT EXISTS patients ("
@@ -31,43 +47,54 @@ const char* creatingTables = "CREATE TABLE IF NOT EXISTS patients ("
 "	password text"
 ");";
 
+std::wstring charToWstring(char * _) {
+    std::string __ = std::string(_);
+    std::wstring ___;
+    ___.assign(__.begin(), __.end());
+    return ___;
+}
+
 const char* getPatientsDataSql = "SELECT * FROM patients";
 int getPatientsData(void* _, int argc, char** argv, char** azColName) {
     for (int i = 1; i <= argc; i += 3) {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-        std::cout << azColName[i - 1] << " " << argv[i - 1] << std::endl;
+        _tempdb_.push_back(new Patient(atoi(argv[i - 1]), charToWstring(argv[i]), charToWstring(argv[i + 1]), Diagnosis()));
+        /*std::cout << azColName[i - 1] << " " << argv[i - 1] << std::endl;
         std::cout << azColName[i] << " " << argv[i] << std::endl;
-        std::cout << azColName[i + 1] << " " << argv[i + 1] << std::endl;
+        std::cout << azColName[i + 1] << " " << argv[i + 1] << std::endl;*/
     }
-    std::cout << std::endl;
+    //std::cout << std::endl;
     return 0;
 }
 
 const char* getDiagnosesDataSql = "SELECT * FROM diagnoses";
 int getDiagnosesData(void* _, int argc, char** argv, char** azColName) {
     for (int i = 1; i <= argc; i+=5) {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-        std::cout << azColName[i - 1] << " " << argv[i - 1] << std::endl;
+        for (int j = 0; j < _tempdb_.size(); ++j) {
+            if (atoi(argv[i]) == _tempdb_[j]->getId()) {
+                _tempdb_[i]->pushDiagnosis(Diagnosis(charToWstring(argv[i + 1]), charToWstring(argv[i + 2]), atoi(argv[i + 3])));
+            }
+        }
+        /*std::cout << azColName[i - 1] << " " << argv[i - 1] << std::endl;
         std::cout << azColName[i] << " " << argv[i] << std::endl;
         std::cout << azColName[i + 1] << " " << argv[i + 1] << std::endl;
         std::cout << azColName[i + 2] << " " << argv[i + 2] << std::endl;
-        std::cout << azColName[i + 3] << " " << argv[i + 3] << std::endl;
+        std::cout << azColName[i + 3] << " " << argv[i + 3] << std::endl;*/
     }
-    std::cout << std::endl;
+    //std::cout << std::endl;
     return 0;
 }
 
 const char* getWorkersDataSql = "SELECT * FROM workers";
 int getWorkersData(void* _, int argc, char** argv, char** azColName) {
     for (int i = 1; i <= argc; i += 5) {
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-        std::cout << azColName[i - 1] << " " << argv[i - 1] << std::endl;
+        _tempdb_.push_back(new Worker(atoi(argv[i - 1]), charToWstring(argv[i]), charToWstring(argv[i+1]), std::string(argv[i + 2]), std::string(argv[i + 3])));
+        /*std::cout << azColName[i - 1] << " " << argv[i - 1] << std::endl;
         std::cout << azColName[i] << " " << argv[i] << std::endl;
         std::cout << azColName[i + 1] << " " << argv[i + 1] << std::endl;
         std::cout << azColName[i + 2] << " " << argv[i + 2] << std::endl;
-        std::cout << azColName[i + 3] << " " << argv[i + 3] << std::endl;
+        std::cout << azColName[i + 3] << " " << argv[i + 3] << std::endl;*/
     }
-    std::cout << std::endl;
+    //std::cout << std::endl;
     return 0;
 }
 
@@ -87,7 +114,7 @@ DB::DB() {
             std::cerr << "Failed to exec: " << rc << ", " << sqlite3_errmsg(db) << std::endl;
             break;
         }
-        if (SQLITE_OK != (rc = sqlite3_exec(db, getPatientsDataSql, getPatientsData, NULL, NULL))) {
+        if (SQLITE_OK != (rc = sqlite3_exec(db, getWorkersDataSql, getWorkersData, NULL, NULL))) {
             std::cerr << "Failed to exec: " << rc << ", " << sqlite3_errmsg(db) << std::endl;
             break;
         }
@@ -95,10 +122,10 @@ DB::DB() {
             std::cerr << "Failed to exec: " << rc << ", " << sqlite3_errmsg(db) << std::endl;
             break;
         }
-        if (SQLITE_OK != (rc = sqlite3_exec(db, getWorkersDataSql, getWorkersData, NULL, NULL))) {
+        if (SQLITE_OK != (rc = sqlite3_exec(db, getPatientsDataSql, getPatientsData, NULL, NULL))) {
             std::cerr << "Failed to exec: " << rc << ", " << sqlite3_errmsg(db) << std::endl;
             break;
-        }
+        }        
     } while (false);
     if (NULL != db) sqlite3_close(db);
     sqlite3_shutdown();
--- Worker/Worker.h
+++ Worker/Worker.h
@@ -1,6 +1,8 @@
 ﻿#include "..\sqlite\sqlite3.h"
 #include <string>
 #include <iostream>
+#include <vector>
+#include "sha256.h"
 
 #ifdef WORKER_EXPORTS
 #define WORKER_API __declspec(dllexport)
@@ -8,34 +10,56 @@
 #define WORKER_API __declspec(dllimport)
 #endif
 
+struct WORKER_API Diagnosis {
+	std::wstring text;
+	std::wstring date;
+	int workerid;
+	Diagnosis(std::wstring text = L"", std::wstring date = L"", int workerid = 0) {
+		this->date = date;
+		this->text = text;
+		this->workerid = workerid;
+	}
+};
+
 class WORKER_API Man {
 private:
-
 public:
 	Man();
 	virtual ~Man();
+	virtual int getId();
+	virtual void pushDiagnosis(Diagnosis);
 };
 
 class WORKER_API Patient : public Man {
 private:
-
+	int id;
+	std::wstring name;
+	std::wstring dob;
+	std::vector<Diagnosis> a;
 public:
-	Patient();
+	Patient(int, std::wstring, std::wstring, Diagnosis);
 	~Patient();
+	int getId() override;
+	void pushDiagnosis(Diagnosis) override;
 };
 
 class WORKER_API Worker : public Man {
 private:
-
+	int id;
+	std::wstring name;
+	std::wstring job;
+	std::string login;
+	std::string password;
 public:
-	Worker();
+	Worker(int, std::wstring, std::wstring, std::string, std::string);
 	~Worker();
+	int getId() override;
+	void pushDiagnosis(Diagnosis) override;
 };
 
 class WORKER_API DB {
 private:
-	const char* dbname = "card.db";
-	sqlite3* db;
+	std::vector<Man*> db;
 public:
 	DB();
 	~DB();
